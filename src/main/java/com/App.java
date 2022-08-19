e873b0e39e8118dd310364c3b5ce80955bf48f41
tejas <tejasshah@gmail.com>
2022-08-19 11:33:37 +0530

    ioc inject userdao

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,15 +1,28 @@
 package com;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.dao.UserDao;
 
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Welcome..");
 
-		UserDao userDao1 = new UserDao();
-		UserDao userDao2 = new UserDao();
+//		UserDao userDao1 = new UserDao();
+//		UserDao userDao2 = new UserDao();
+//
+//		System.out.println(userDao1);
+//		System.out.println(userDao2);
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		System.out.println(context);
+
+		UserDao userDao1 = context.getBean("userDao", UserDao.class);
+		UserDao userDao2 = context.getBean("userDao", UserDao.class);
+		UserDao userDao3 = context.getBean("userDao", UserDao.class);
 
 		System.out.println(userDao1);
 		System.out.println(userDao2);
+		System.out.println(userDao3);
 	}
 }
